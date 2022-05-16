f98bc42841ea1044f99d5d65e501630f49e74242
Fellipe Henrique <fellipehf@gmail.com>
2022-05-15 21:55:22 -0300

    fix some files to deployment

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app';
 
-const url = process.env.APP_URL || 'http://localhost:';
+const url = process.env.APP_URL || 'http://localhost';
 const port = process.env.APP_PORT || 3000;
 
 app.listen(port, () => {
